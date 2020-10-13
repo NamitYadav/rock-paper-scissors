fa229d7cce505808214e34ee548fbd6ef3952d3e
Namit Yadav <nyadav@empyra.com>
2020-10-13 13:10:06 +0530

    Updates

---
+++ precache-manifest.20c8603d4d2aac0b1a37d9ce006393e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3355f06d9f20929785cad0166ac6d1ab",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "e0e756767401b091326d",
+    "url": "/rock-paper-scissors/static/css/main.fd5d88ca.chunk.css"
+  },
+  {
+    "revision": "dac0d34737fa997ef297",
+    "url": "/rock-paper-scissors/static/js/2.15709ae6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors/static/js/2.15709ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e756767401b091326d",
+    "url": "/rock-paper-scissors/static/js/main.a7e2e09d.chunk.js"
+  },
+  {
+    "revision": "54d8b3ee6fc233b06722",
+    "url": "/rock-paper-scissors/static/js/runtime-main.5af9a2bb.js"
+  }
+]);
